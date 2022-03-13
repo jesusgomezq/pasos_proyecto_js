b435a725f545f91a3ac000cef55dbbaf52bd710e
jesusgomez <jgomez4543@gmail.com>
2022-03-13 03:02:17 -0300

    queriendo captar datos

--- js/validacion.js
+++ js/validacion.js
@@ -1,4 +1,4 @@
-let reservas = document.getElementById("reservas");
+let reservas = document.getElementById("reservas", "nombre");
 
 reservas.addEventListener("submit", function (event) {
   console.log("Dentro");
@@ -8,13 +8,24 @@ reservas.addEventListener("submit", function (event) {
 let checkin = document.getElementById("checkin");
 let checkout = document.getElementById("checkout");
 let mensaje = document.getElementById("mensaje");
+let correto = false;
 
 checkout.addEventListener("change", function () {
   //   console.log(checkout);
   if (checkin.type == checkout.type) {
-    checkout.type = null;
-    alert("elije una fecha distinta");
-  } else {
-    alert("bienvenido");
+    checkin = null;
+    mensaje.innerHTML = "<p>elige otra fecha de CKECKOUT</p>";
+    mensaje.style.color = "red";
+  }
+  if (checkin.type != checkout.type) {
+    checkout.type = true;
+    mensaje.innerHTML = "<p> Bienvenido come pinga</p>";
+    mensaje.style.color = "green";
   }
 });
+
+// // captura de datos de formulario
+function captarDatos() {
+  let datos = document.getElementById("nombre").value;
+  alert(datos);
+}
