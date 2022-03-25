9acdaa9c89dec678cf0f4245a6579a35c35a2d01
jesusgomez <jgomez4543@gmail.com>
2022-03-24 23:40:25 -0300

    desastres en arrays de objetos

--- js/validacion.js
+++ js/validacion.js
@@ -2,6 +2,7 @@
 let formulario = document.getElementById("reservas");
 let btn_reservar = document.getElementById("enviar");
 let mensaje = document.getElementById("mensaje");
+let aReservas = [];
 
 function invalido() {
   mensaje.innerText = "Debes completar todos los campos solicitados";
@@ -27,44 +28,88 @@ document.getElementById("checkin").addEventListener("change", () => {
 
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
-  // usamos "Date" para trabajar con fechas
+  function Reserva(
+    chechin,
+    checkout,
+    precioPorNoche,
+    hotel,
+    cantidadPersonas,
+    cantidadDias,
+    total
+  ) {
+    this.chechin = chechin;
+    this.checkout = checkout;
+    this.precioPorNoche = precioPorNoche;
+    this.hotel = hotel;
+    this.cantidadPersonas = cantidadPersonas;
+    this.cantidadDias = cantidadDias;
+    this.total = total;
+  }
 
   let checkin = new Date(document.getElementById("checkin").value);
   let checkout = new Date(document.getElementById("checkout").value);
 
   let cantidadPersonas = parseInt(document.getElementById("personas").value);
-  let precioPornoche = 3000;
+  let hotel = document.getElementById("hotel").value;
+  let precioPorNoche;
 
-  // mientras la fecha de salida es igual o menos a la de entrada, entonces mandamos un mnj de error.
+  switch (hotel) {
+    case "ecoland":
+      precioPorNoche = 3500;
+
+      break;
+    case "hesperia":
+      precioPorNoche = 3300;
+      break;
+    case "isla-caribe":
+      precioPorNoche = 3000;
+      break;
+    default:
+      break;
+  }
+  // mientras la fecha de salida es igual o menor a la de entrada, entonces mandamos un mnj de error.
   if (checkout <= checkin || checkout === checkin) {
     mensaje.innerHTML =
-      "<p> La fecha de entrada, pued ser igual que la de salida.</p>";
+      "<p> La fecha de entrada, no puede ser igual a la de salida.</p>";
     mensaje.style.color = "red";
+
     return;
   }
   // con una funcion calculamos los dias que elije el usuario
   let cantidadDias = calcularCantidadDias(checkin, checkout);
 
-  let total = cantidadPersonas * precioPornoche * cantidadDias;
+  let total = cantidadPersonas * precioPorNoche * cantidadDias;
+
+  // aca en esta linea creamos una vanriable de tipo reserva.
+  aReserva = new Reserva(
+    checkin,
+    checkout,
+    precioPorNoche,
+    hotel,
+    cantidadPersonas,
+    cantidadDias,
+    total
+  );
+  console.log(Areservaer);
+  let baseDeDatos = [];
+  function agregar() {
+    console.log("capturado");
+  }
+
+  aReservas.push(Reserva);
   Swal.fire(
     "Reserva exitosa",
     "Enviaremos un correo con el detalle!",
     "success"
   );
-  alert(
-    "Cantidad de personas: " +
-      cantidadPersonas +
-      "\n" +
-      "Cantidad de noches: " +
-      cantidadDias +
-      "\n" +
-      "Total: " +
-      total
-  );
   formulario.reset();
   mensaje.innerHTML = "<p>Formulario enviado correctamente</p>";
   mensaje.style.color = "green";
 });
+
+function cerrar() {
+  mensaje.innerHTML = "";
+}
 
 // funcion hecha para mostrar diferencias entre la entrada y la salida
 
